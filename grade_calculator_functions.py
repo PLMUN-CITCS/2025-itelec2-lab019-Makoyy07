369dd8fe0487625f522d83b99cbe1339104a8267
Makoyy07 <>
2025-02-24 22:05:04 +0800

    Last Sync: 2025-02-24 14:05:04 (Mobile)

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -1,5 +1,5 @@
 def get_student_score() -> float:
-   
+  
     while True:
         try:
             score = float(input("Enter your score: "))
@@ -8,23 +8,23 @@ def get_student_score() -> float:
             else:
                 print("Please enter a score between 0 and 100.")
         except ValueError:
-            print("Invalid input. Please enter a numeric value.")
+            print("Invalid input. Please enter a numerical value.")
 
 def calculate_grade(score: float) -> str:
-   
-    grades = {
-        (90, 100): 'A',
-        (80, 89): 'B',
-        (70, 79): 'C',
-        (60, 69): 'D'
-    }
-    for range_, grade in grades.items():
-        if range_[0] <= score <= range_[1]:
-            return grade
-    return 'F'
+
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+
 
 def main():
-  
     score = get_student_score()
     grade = calculate_grade(score)
     print(f"Your Grade is: {grade}")
